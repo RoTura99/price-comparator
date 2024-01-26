22ccced09c715bad78698d8d44e3b121364fda89
RoTura99 <tura.robert@gmail.com>
2024-01-26 18:07:28 +0100

    new example

--- src/utils.py
+++ src/utils.py
@@ -5,15 +5,11 @@ import streamlit as st
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+    
+    return output
 
 def get_plantilla():
     d = {
