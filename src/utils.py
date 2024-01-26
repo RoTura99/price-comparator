30417606d5522a613983b533845744efdb304e85
RoTura99 <tura.robert@gmail.com>
2024-01-26 18:12:52 +0100

    fix version issue

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def to_excel(df):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
+        writer.close()
     
     return output
 
