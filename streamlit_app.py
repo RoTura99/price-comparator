333faa0f7179eb285497e10b59639a6a83fb8a1e
RoTura99 <tura.robert@gmail.com>
2024-01-27 23:59:19 +0100

    fix issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,23 +19,27 @@ with st.sidebar:
     st.download_button("Plantilla competencia", plantilla_competidores,  "plantilla_competidores.xlsx", help = "Descarga la plantilla para subir los precios de la competencia")
     competencia_raw = st.file_uploader("Precios Competencia", type="xlsx")
     if competencia_raw is not None:
-        competencia_excel = pd.read_excel(competencia_raw)
-        competidores = clean_list(competencia_excel.columns.tolist())
+        competencia_df = pd.read_excel(competencia_raw)
+        competidores = clean_list(competencia_df.columns.tolist())
     else:
         competidores = []
     st.download_button("Plantilla propia", plantilla_propia,  "plantilla_propia.xlsx", help = "Descarga la plantilla para subir tus precios")
     propios_raw = st.file_uploader("Precios Propios", type="xlsx")
     if propios_raw is not None:
-        propios_excel = pd.read_excel(propios_raw)
+        propios_df = pd.read_excel(propios_raw)
     main_competidor = st.multiselect("Selecciona el competidor principal", competidores)
 
 st.title("Price Comparator")
 #make one round of prices to generate ideas of the app, make the download button work and compare.
-main_price = 1
-average_price = 1
-median_price = 1
-st.write(competencia_excel.columns)
 if competencia_raw is None or propios_raw is None:
     st.write("Sube los archivos para poder comparar")
 else:
-    st.dataframe(competencia_excel)
+    precios = pd.merge(propios_df, competencia_df, how = 'left', on=["SKU", "Nombre de Producto"])
+    if main_competidor == []:
+        precios['Precio Principal'] = 0
+    else:
+        precios['Precio Principal'] = precios[main_competidor]
+    precios['Precio Promedio'] = precios[competidores].mean(axis=1)
+    precios['Precio Mediano'] = precios[competidores].median(axis=1)
+    st.data_editor(precios)
+    st.download_button("Descarga los precios", precios,  "precios_final.xlsx", help = "Descarga la hoja de precios final")
