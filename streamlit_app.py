c7adcc5d47d87a6069eca19bec790d03ea1cec9a
RoTura99 <tura.robert@gmail.com>
2024-01-27 23:45:54 +0100

    fix issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,16 +20,21 @@ with st.sidebar:
     competencia_raw = st.file_uploader("Precios Competencia", type="xlsx")
     if competencia_raw is not None:
         competencia_excel = pd.read_excel(competencia_raw)
-    #     competidores = clean_list(competencia_raw.columns)
-    # else:
-    #     competidores = []
+        competidores = clean_list(competencia_excel.columns)
+    else:
+        competidores = []
     st.download_button("Plantilla propia", plantilla_propia,  "plantilla_propia.xlsx", help = "Descarga la plantilla para subir tus precios")
     propios_raw = st.file_uploader("Precios Propios", type="xlsx")
-    # main_competidor = st.multiselect("Selecciona el competidor principal", competidores)
+    if propios_raw is not None:
+        propios_excel = pd.read_excel(propios_raw)
+    main_competidor = st.multiselect("Selecciona el competidor principal", competidores)
 
 st.title("Price Comparator")
 #make one round of prices to generate ideas of the app, make the download button work and compare.
 main_price = 1
 average_price = 1
 median_price = 1
-st.dataframe(competencia_excel)
+if competencia_raw is None or propios_raw is None:
+    st.write("Sube los archivos para poder comparar")
+else:
+    st.dataframe(competencia_excel)
